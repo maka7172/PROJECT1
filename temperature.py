ecb786c435848d331b80a0d5646ee24cfae481da
maka7172 <kholghahmadi62@gmail.com>
2023-09-05 22:38:14 +0330

    temperature app the end

--- temperature.py
+++ temperature.py
@@ -1,8 +1,14 @@
 import tkinter as tk
 from tkinter import TOP,BOTTOM,RIGHT
+import string
+
+# def check_page() :
+#     if entery_give_faren.get() != True :
+#         result_label['text'] = 'the temperature is empty!!'
 
 window1 = tk.Tk()
 window1.title('Conver Temperature')
+
 farenhait_label = tk.Label(master=window1,
                            height=3,
                            font="TOHOMA",
@@ -19,9 +25,20 @@ celsus_label = tk.Label(master=window1,
 result_label = tk.Label(master=window1,
                         text='result will be here',
                         )
-def calc_far_to_cel() :
-    result_label['text'] = f"the result is :{(float(entery_give_faren.get())-32)*5.9 }"
 
+
+def calc_far_to_cel(*args) :
+    ent_input = entery_give_faren.get()
+    try :
+        ent_value = float(ent_input)
+        result_label['text'] = f"the result is :{ent_value}"
+    except ValueError :
+        if (ent_input) != '' :
+            result_label['text'] = "you shuld enter valid data"
+        else :
+            result_label['text'] = "the input is empty!!"
+        
+window1.bind('<Return>' , calc_far_to_cel)
 button_calc = tk.Button(master=window1,
                         text='calc!!',
                         command=calc_far_to_cel,
@@ -32,5 +49,8 @@ entery_give_faren.grid(row=0,column=1,sticky=('W',))
 button_calc.grid(row=0,column=2,sticky=("w",))
 celsus_label.grid(row=2,column=0)
 result_label.grid(row=2,column=1,columnspan=2)
-    
+
+if entery_give_faren.get() != True :
+    result_label['text'] = 'the temperature is empty!!'
+
 window1.mainloop()
